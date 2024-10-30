05c08ffba848199ab307fcd00f24bcd2b76c2c5f
FurkannOzbek <furkannozbek@yandex.com>
2024-10-30 23:27:55 +0100

    logic for errors added

--- mortgage-repayment-calculator/app/Components/MortgageCard.jsx
+++ mortgage-repayment-calculator/app/Components/MortgageCard.jsx
@@ -5,26 +5,80 @@ import { useEffect } from "react";
 export default function MortgageCard() {
   const [formatAmount, setFormatAmount] = useState("");
   const [amount, setAmount] = useState(1);
+  const [rate, setRate] = useState("");
+  const [years, setYears] = useState("");
+  const [yearsErrorMessage, setYearsErrorMessage] = useState("");
+  const [rateErrorMessage, setRateErrorMessage] = useState("");
+  const [amountErrorMessage, setAmountErrorMessage] = useState("");
+  const [isRateError, setIsRateError] = useState(false);
+  const [isYearsError, setIsYearsError] = useState(false);
+  const [isAmountError, setIsAmountError] = useState(false);
 
   function formSubmit(e) {
+    if (years == "" || years == undefined) {
+      setYearsErrorMessage("This field is required");
+      setIsYearsError(true);
+      setYears("");
+      console.log(years);
+      console.log("error message", yearsErrorMessage);
+    }
+    if (rate == "" || rate == undefined) {
+      setRateErrorMessage("This field is required");
+      setIsRateError(true);
+      setRate("");
+    }
+    if (amount == "" || amount == undefined) {
+      setAmountErrorMessage("This field is required");
+      setIsAmountError(true);
+      setAmount("");
+    }
     e.preventDefault();
-    console.log("form submitted");
+    console.log("submit");
+    console.log(rate);
+    console.log(years);
   }
-  // Formating the Currency
+  // Function for Formating the Currency
   const formatCurrency = (numberString) => {
     const formattedNumber = numberString
       .replace(/[^0-9.]/g, "") // Remove non-digit and non-dot characters
       .replace(/(\..*)\./g, "$1") // Remove all dots except the first one
       .replace(/^(\d+\.?\d{0,2}).*/, "$1"); // Limit to two digits after the dot
-    // Format the number with commas
-    const formattedValue = formattedNumber.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    const formattedValue = formattedNumber.replace(/\B(?=(\d{3})+(?!\d))/g, ","); // Format the number with commas
     return formattedValue;
+  };
+
+  // Format Years
+  const formatYears = (inputYears) => {
+    if (inputYears > 30 || inputYears < 0) {
+      setYearsErrorMessage("Please type an input between 1-30");
+      setIsYearsError(true);
+    }
+    // In default, when delete the amount from term input, if you delete all it sets the year as 0 , this code to prevent
+    if (inputYears == "") {
+      return "";
+    }
+    const formattedYears = inputYears.replace(/[^0-9]/g, "");
+    const years = Math.min(formattedYears, 30);
+    console.log("years", years);
+    return years.toString();
+  };
+  // Formating Rate
+  const formatRate = (inputRate) => {
+    if (inputRate > 100 || inputRate < 0) {
+      setRateErrorMessage("Please type an input between 0-100");
+      setIsRateError(true);
+    }
+    if (inputRate == "") {
+      return "";
+    }
+    const formattedRate = inputRate.replace(/[^0-9]/g, "");
+    const rate = Math.min(formattedRate, 100);
+    return rate.toString();
   };
 
   // Handle the Input Change
   const handleCurrencyInputChange = (e) => {
     let inputValue = e.target.value;
-    // Format the input value
     let formattedValue = formatCurrency(inputValue);
     // Remove leading zero if present and not just '0'
     if (formattedValue.length > 0 && formattedValue[0] === "0" && formattedValue !== "0") {
@@ -35,7 +89,28 @@ export default function MortgageCard() {
     setAmount(valueWithoutComas);
     setFormatAmount(formattedValue);
   };
-
+  const handleYearsInputChange = (e) => {
+    setIsYearsError(false); // Reset the error situation if new number entered
+    const years = e.target.value;
+    if (years === undefined || years == 0) {
+      setIsYearsError(false);
+      setYears("");
+    }
+    const formattedYears = formatYears(years);
+    setYears(formattedYears);
+  };
+  // Handle Rate Change
+  const handleRateInputChange = (e) => {
+    setIsRateError(false); // Reset the error situation if new rate entered
+    const rate = e.target.value;
+    console.log(rate);
+    if (rate === undefined || rate == 0) {
+      setIsRateError(false);
+      setRate("");
+    }
+    const formattedRate = formatRate(rate);
+    setRate(formattedRate);
+  };
   return (
     <>
       <div className={styles.mortgageCard}>
@@ -61,15 +136,33 @@ export default function MortgageCard() {
             <div className={styles.inputWrapperTermAndRate}>
               {/* Container for Term Input*/}
               <div className={styles.mortgageTermContainer}>
-                <label className={styles.inputLabel}>
-                  Mortgage Term
-                  {/* Overriding css for not creating another class */}
-                </label>
+                <label className={styles.inputLabel}>Mortgage Term</label>
                 {/* Input Wrapper for input and span */}
                 <div className={styles.inputWrapper}>
-                  <input className={`${styles.inputField} ${styles.yearsInputField}`} />
-                  <span className={styles.yearsIcon}> years </span>
+                  <input
+                    value={years}
+                    className={
+                      !isYearsError
+                        ? `${styles.inputField} ${styles.yearsInputField}`
+                        : `${styles.inputField} ${styles.yearsInputField} ${styles.yearsError}`
+                    }
+                    onChange={handleYearsInputChange}
+                  />
+                  <span
+                    className={
+                      !isYearsError
+                        ? `${styles.yearsIcon}`
+                        : `${styles.yearsIcon} ${styles.yearsIconError}`
+                    }
+                  >
+                    years
+                  </span>
                 </div>
+                {!isYearsError ? (
+                  <div></div>
+                ) : (
+                  <span className={styles.yearsErrorMessage}> {yearsErrorMessage}</span>
+                )}
               </div>
               {/* Container for Rate Input*/}
               <div className={styles.interestRateContainer}>
@@ -78,11 +171,34 @@ export default function MortgageCard() {
                 </label>
                 {/* Input Wrapper for input and span */}
                 <div className={styles.inputWrapper}>
-                  <input className={`${styles.inputField} ${styles.rateInputField}`} />
-                  <span className={styles.rateIcon}> % </span>
+                  <input
+                    value={rate}
+                    className={
+                      !isRateError
+                        ? `${styles.inputField} ${styles.rateInputField}`
+                        : `${styles.inputField} ${styles.rateInputField} ${styles.rateError}`
+                    }
+                    onChange={handleRateInputChange}
+                  />
+                  <span
+                    className={
+                      !isRateError
+                        ? `${styles.rateIcon}`
+                        : `${styles.rateIcon} ${styles.rateIconError}`
+                    }
+                  >
+                    {" "}
+                    %{" "}
+                  </span>
                 </div>
+                {!isRateError ? (
+                  <div></div>
+                ) : (
+                  <span className={styles.rateErrorMessage}> {rateErrorMessage}</span>
+                )}
               </div>
             </div>
+            <button type="submit" onSubmit={formSubmit}></button>
           </form>
         </div>
         <div className={styles.resultTab}></div>
